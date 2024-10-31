0c83f9fd66f66eee2d5a9d300d2e22e8f02bcb24
Supriyo Saha <mm.saha1982@gmail.com>
2024-10-31 17:13:59 +0530

    fix

--- advance-concepts/server/trpc.ts
+++ advance-concepts/server/trpc.ts
@@ -1,12 +1,12 @@
-import { initTRPC } from '@trpc/server';
+import { initTRPC } from "@trpc/server";
 import { Todo, User } from "./db";
- 
 /**
  * Initialization of tRPC backend
  * Should be done only once per backend!
  */
-const t = initTRPC.context<{db: {Todo: typeof Todo, User: typeof User}; userId?: string;}>().create();
- 
+const t = initTRPC
+  .context<{ db: { Todo: typeof Todo; User: typeof User }; userId?: string }>()
+  .create();
 /**
  * Export reusable router and procedure helpers
  * that can be used throughout the router
